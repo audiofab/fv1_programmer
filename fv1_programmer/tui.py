3093cea6393f4b7e979602076a0edaa64c806ac5
Mark Melvin <mark.melvin@audiofab.com>
2024-04-09 21:29:08 -0400

    Bump version

--- fv1_programmer/tui.py
+++ fv1_programmer/tui.py
@@ -39,7 +39,7 @@ from fv1_programmer.fv1 import FV1Program, FV1_PROGRAM_MAX_BYTES
 from fv1_programmer.dialogs import *
 
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 _title = "FV1 Programmer"
 MIN_PROGRAM_NUM = 1
